bb0fbc9a003af01d06be943628c75cc7f8131ddd
Kyrylo Stiopin <skyrylo@amazon.com>
2022-05-09 14:01:38 +0100

    Added example how to use third-party libraries in first post

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,18 +1,30 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Script from 'next/script';
 
-const FirstPost = () => (
-  <>
-    <Head>
-      <title>First Post</title>
-    </Head>
-    <h1>First Post</h1>
-    <h2>
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
-    </h2>
-  </>
-);
+const FirstPost = () => {
+  const onScriptLoad = () => {
+    console.log(`script loaded correctly, window.FB has been populated`);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={onScriptLoad}
+      />
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
+};
 
 export default FirstPost;
