2a74350019cfda92bf43e0a4cab0f40e587a23f1
Kyrylo Stiopin <skyrylo@amazon.com>
2022-05-09 14:04:24 +0100

    An example of using images

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,6 +1,9 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 import Script from 'next/script';
+
+import profilePic from '../../public/images/profile.jpeg';
 
 const FirstPost = () => {
   const onScriptLoad = () => {
@@ -18,6 +21,12 @@ const FirstPost = () => {
         onLoad={onScriptLoad}
       />
       <h1>First Post</h1>
+      <Image
+        src={profilePic}
+        alt="Picture of the author"
+        width={50}
+        height={50}
+      />
       <h2>
         <Link href="/">
           <a>Back to home</a>
