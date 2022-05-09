3e529afe3a313d36ecdc6d9e65db5ebf61e84927
Kyrylo Stiopin <skyrylo@amazon.com>
2022-05-09 15:24:08 +0100

    Update Layout component to have a wrapper around all pages

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,138 +3,43 @@ import Head from 'next/head';
 import Link from 'next/link';
 
 import ExampleGrid from '../components/ExampleGrid';
+import Layout, { siteTitle } from '../components/layout';
+import mainStyles from '../styles/main.module.css';
+import utilStyles from '../styles/utils.module.css';
 
 const HomePage = () => (
-  <div className="container">
+  <Layout home>
     <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
+      <title>{siteTitle}</title>
     </Head>
-
-    <main>
-      <h1 className="title">
-        Read{' '}
-        <Link href="/posts/first-post">
-          <a>this page!</a>
-        </Link>
+    <section className={utilStyles.headingMd}>
+      <h1 className={mainStyles.title}>
+        Getting familiar with NextJS and it's core features.
       </h1>
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
+      <p className={mainStyles.description}>
+        (This is a sample website - you'll be building a site like this on{' '}
+        <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
       </p>
+    </section>
+
+    <main className={mainStyles.main}>
+      <Link href="/posts/first-post">
+        Example page
+      </Link>
+
+      <h2 className={mainStyles.title}>
+        Below is a list of useful resources.
+      </h2>
 
       <ExampleGrid />
     </main>
 
     <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer">
-        Powered by{' '}
-        <img src="/vercel.svg" alt="Vercel" className="logo" />
-      </a>
+      (This is a sample website - you'll be building a site like this on{' '}
+      <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
     </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .logo {
-        height: 1em;
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-          Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-          sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
+  </Layout>
 );
 
 export default HomePage;
--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,10 +1,8 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import Link from 'next/link';
 import Script from 'next/script';
 
 import Layout from '../../components/Layout';
-import profilePic from '../../public/images/profile.jpeg';
 
 const FirstPost = () => {
   const onScriptLoad = () => {
@@ -22,17 +20,7 @@ const FirstPost = () => {
         onLoad={onScriptLoad}
       />
       <h1>First Post</h1>
-      <Image
-        src={profilePic}
-        alt="Picture of the author"
-        width={50}
-        height={50}
-      />
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+      <h2>Has Facebook SDK available</h2>
     </Layout>
   );
 };
